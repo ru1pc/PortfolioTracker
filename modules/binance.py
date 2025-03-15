13115fc61f9f2f8bc64202ab0b6075d1102be1dd
ru1pc <ru1pc@work.com>
2025-03-15 18:50:54 +0000

    add basic tests; ci updated; modules updated

--- modules/binance.py
+++ modules/binance.py
@@ -1,55 +1,41 @@
+from utils import logger
 from datetime import datetime
-
 import pandas as pd
 
 
-def clean_data_order(df):
-    drop_columns = ['OrderNo', 'Order Price','Trigger Condition', 'Filled', 'status']
-    df = df.drop(labels=drop_columns, axis=1)
+def rename_columns(df, column_mapper):
 
-    column_mapper = {
-        'Date(UTC)': 'Date',
-        'Pair': 'Asset',
-        'AvgTrading Price': 'Price',
-        'Order Amount': 'Amount',
-        'Total': 'Cost'
-    }
+    existing_columns = {k: v for k, v in column_mapper.items() if k in df.columns}
+    missing_columns = [k for k in column_mapper if k not in df.columns]
 
-    df = df.rename(mapper=column_mapper, axis=1)
+    if missing_columns:
+        logger.warning(f"⚠️ Columns not found for renaming: {missing_columns}")
 
-    df.columns = df.columns.str.title()
+    return df.rename(columns=existing_columns)
 
-    pattern = r'([A-Z]+)(USDT|USDC|USD|EURI|EUR)'
-
-    df[['Asset','Currency']] = df['Asset'].str.extract(pattern)
-
-    # Convert the column to datetime (Pandas will handle any format discrepancies)
-    df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
-    df['Date'] = df['Date'].dt.strftime("%d/%m/%Y")
-
-    df[['Amount','Price','Total']] = df[['Amount','Price','Total']].round(8)
-
-    return df
 
 def clean_data(df):
+
     column_mapper = {
         'Date(UTC)': 'Date',
         'Market': 'Asset',
         'Total': 'Cost'
     }
-
-    print(df)
-    df = df.rename(mapper=column_mapper, axis=1)
+    df = rename_columns(df, column_mapper)
+    #df = df.rename(mapper=column_mapper, axis=1)
+    
     pattern = r'([A-Z]+)(USDT|USDC|USD|EURI|EUR)'
 
-
     df[['Date', 'Time(UTC)']] = df['Date'].astype(str).str.split(' ', expand=True)
-    # Extract year and month for filtering
+    
+    #Extract year and month for filtering
     df['Date'] = pd.to_datetime(df['Date'])
-    df['Year'] = df['Date'].dt.year
-    df['Month'] = df['Date'].dt.month
-    df['Date'] = df['Date'].dt.date
-
+    df = df.assign(
+        Year=df['Date'].dt.year,
+        Month=df['Date'].dt.month,
+        Date=df['Date'].dt.date
+    )
+    
     df[['Asset','Currency']] = df['Asset'].str.extract(pattern)
 
     #df = df.astype({'Asset': 'category', 'Price': 'float64', 'Amount':'float64', 'Cost': 'float64', 'Fee': 'float64','Platform':'category'})
